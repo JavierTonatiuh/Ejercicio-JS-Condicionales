0920f411b868b1bde51734f29662ae040f9e7f0a
JavierTonatiuh <javiertonatiuh77@gmail.com>
2024-07-03 12:22:36 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+/*let edad = 14;
+
+if (edad>=18){
+    console.log("Eres mayor de edad")
+
+} else {
+    console.log("Eres menor de edad")
+
+}
+
+let dispositivo = prompt("Como quieres ver netflix, laptop, telefono, pantalla")
+
+if(dispositivo == "telefono"){
+    console.log("Entra a google y escribe netflix")
+
+
+} else if(dispositivo == "pantalla"){
+    console.log("Prende tv y presiona el simbolo")
+
+} else if(dispositivo == "laptop"){
+    console.log("Entra a internet")
+
+} else{
+    console.log("no puedes ver netflix")
+}*/
+
+/*let figura = prompt("Cual es tu figura, triangulo/cuadrado/circulo");
+
+function areaTriangulo(base, altura){
+    return (base * altura)/2;
+}
+
+function areaCuadrado(lado){
+    return lado * lado;
+}
+
+function areaCirculo(pi, radio){
+    return (pi * radio)
+}
+
+if(figura == "triangulo"){
+    let base = parseFloat(prompt("Cuanto mide tu base"))
+    let altura = parseFloat(prompt("Cual es tu altura"))
+    alert ("El area del triangulo es: " + areaTriangulo(base, altura))
+
+}else if(figura == "cuadrado"){
+    let lado = parseFloat(prompt("Cuanto mide tu lado"))
+    alert ("El area de tu cuadrado es: " + areaCuadrado(lado))
+}else if(figura == "circulo"){
+    let pi = 13.1416;
+    let radio = parseFloat(prompt("Cuanto mide tu radio"))
+    alert ("El area del circulo es: " + areaCirculo(pi, radio))
+}else{
+    console.log("No puedo calcular esa figura")
+}*/
+
+//let edad = Number(prompt("Cual es tu edad"))
+//let membresia = prompt("Cual es tu membresia? Vip/Plata/Normal")
+
+/*if( membresia == "Vip" && edad >=18){
+    console.log("Tienes permitido el acceso VIP")
+}else if((membresia == "Plata" || "Normal") && edad >=18){
+    console.log("Tienes permitdo el acceso plata y normal")
+
+}*/
+
+
+
+
